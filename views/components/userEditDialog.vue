4d960214e8fdfea8eb8f33d36decc99f68b73346
Martin MacDonald <martin.macdonald@screenmedia.co.uk>
2018-05-03 22:40:30 +0100

    first commit

---
+++ views/components/userEditDialog.vue
@@ -0,0 +1,56 @@
+<template>
+  <v-card>
+    <v-toolbar>
+      <div class="headline"> Edit User: {{editName}} </div>
+    </v-toolbar>
+    <v-container fluid>
+      <v-card-text>
+
+        <!-- Begin Input Row -->
+        <v-form ref="form">
+          <v-text-field label="Name" v-model="changedUser.name"> </v-text-field>
+          <v-slider label="Age" v-model="changedUser.age" thumb-label step="1"></v-slider>
+          <v-text-field label="Email" v-model="changedUser.email" :rules="[rules.email]"> </v-text-field>
+
+          <v-btn @click="$emit('edited', changedUser)" class="green lighten-1 white--text">Submit</v-btn>
+          <v-btn @click="$emit('closeEdit')" class="red white--text">Close</v-btn>
+        </v-form>
+      </v-card-text>
+    </v-container>
+  </v-card>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    changedUser: {
+      name: '',
+      email: '',
+      age: 0
+    }
+
+  }),
+
+  props: {
+    user: {
+      type: Object
+    },
+    rules: {
+      type: Object
+    },
+    editName: {
+      type: String,
+      default: ''
+    }
+  },
+
+  created() {
+    this.changedUser = this.user
+  }
+
+}
+</script>
+
+<style>
+
+</style>
